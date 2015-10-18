641a35cf2b03452c1e783d90b1cb826ce800420e
matisapatta <mati.sapatta@gmail.com>
2015-10-18 20:07:44 -0300

    bueo

---
+++ ConexionBD/test1.js
@@ -0,0 +1,23 @@
+/**
+ * Created by mati on 10/18/15.
+ */
+var params = {
+    /*
+     // optional (for pagination, returned as LastEvaluatedTableName)
+     ExclusiveStartTableName: 'table_name',
+
+     // optional (to further limit the number of table names returned per page)
+     Limit: 1
+     */
+};
+
+var program = require('commander');
+var AWS = require('aws-sdk');
+AWS.config.loadFromPath('./config.json');
+var dynamodb = new AWS.DynamoDB();
+var headers = [];
+
+dynamodb.listTables(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log(data);           // successful response
+});
