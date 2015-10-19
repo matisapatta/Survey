b5b373b66993347906f2f228fefd36997c53aa69
dgagliardi92 <dgagliardi92@gmail.com>
2015-10-19 01:17:33 -0300

    GENERA JSOOON

--- ObtencionDatosPersonal/DatosPersonal.php
+++ ObtencionDatosPersonal/DatosPersonal.php
@@ -13,7 +13,7 @@ class DatosPersonal
     public function get(){
     /*este metodo obtiene los datos de los empleados desde un archivo csv y los guarda en un array de dos dimensiones
         donde cada fila tiene todos los datos del empleado*/
-        $myfile = fopen($this->fileEmpleados, "r") or die("No se puede abrir el archivo!");
+        $myfile = fopen($this->fileEmpleados, "r", 1) or die("No se puede abrir el archivo!");
         $datosEmpleados = [];
         while(!feof($myfile)) {
             $datosEmpleados[] = fgetcsv($myfile);
