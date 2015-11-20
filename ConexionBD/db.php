30be966a27c7d6e39666599f70c8f182923758de
alanarangof <alanarangof@gmail.com>
2015-11-20 19:19:56 -0300

    Alan: modifico db.php para levantar archivo de server ARM y modifico index.html para agregar animacion de loading mientras carga los datos

--- ConexionBD/db.php
+++ ConexionBD/db.php
@@ -9,13 +9,12 @@
 include_once("../Merge/Survey.php");
 class DBData
 {
-    public $fileEmpleados = 'Survey.csv';
+    public $fileEmpleados = "http://212.47.226.113/Survey.csv";
 
     public function get(){
         /*este metodo obtiene los datos de los empleados desde un archivo csv y los guarda en un array de dos dimensiones
             donde cada fila tiene todos los datos del empleado*/
         $myfile = fopen($this->fileEmpleados, "r", 1) or die("No se puede abrir el archivo!");
-
         $arraydtoSurveys = new arraySurvey();
 
         while(!feof($myfile)) {
